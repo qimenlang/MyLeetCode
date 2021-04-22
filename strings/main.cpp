33b4a313edc28de0a31f0d5746dcfcfebe3b09f2
lijiaxiang <jiaxiangli@faceunity.com>
2021-04-22 12:02:51 +0800

    4 pass

--- strings/main.cpp
+++ strings/main.cpp
@@ -6,31 +6,28 @@ using namespace std;
 int lengthOfLongestSubstring(string s) {
 	int minLen = 0;
 	size_t len = s.length();
-	if (len == 1)
-		minLen = 1;
 	for (int i = 0; i < len; i++)
 	{
-		for (int j = i + 1; j < len; j++)
+		for (int j = i + 1; j <= len; j++)
 		{
 			auto subStr = s.substr(i,j - i);
+			if (subStr.length() > minLen)
+				minLen = subStr.length();
 			if (subStr.find(s[j]) != string::npos)
-			{
-				if((subStr.length() > minLen))
-					minLen = subStr.length();
 				break;
-			}
 		}
 	}
+
 	return minLen;
 }
 
 int main()
 {
-	//cout << lengthOfLongestSubstring("abcabcbb") << endl;
-	//cout << lengthOfLongestSubstring("bbbbb") << endl;
-	//cout << lengthOfLongestSubstring("pwwkew") << endl;
-	//cout << lengthOfLongestSubstring("") << endl;
-	//cout << lengthOfLongestSubstring("a") << endl;
+	cout << lengthOfLongestSubstring("abcabcbb") << endl;
+	cout << lengthOfLongestSubstring("bbbbb") << endl;
+	cout << lengthOfLongestSubstring("pwwkew") << endl;
+	cout << lengthOfLongestSubstring("") << endl;
+	cout << lengthOfLongestSubstring("a") << endl;
 	cout << lengthOfLongestSubstring("au") << endl;
 	return 0;
 }
