34137fe5cbba54d9e729bb74f1385d9d45edaf88
lijiaxiang <jiaxiangli@faceunity.com>
2021-04-28 16:09:50 +0800

    resubmit 4

--- strings/main.cpp
+++ strings/main.cpp
@@ -21,6 +21,25 @@ int lengthOfLongestSubstring(string s) {
 	return minLen;
 }
 
+int lengthOfLongestSubstring_2(string s) {
+	int minSize = 0;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		for (int j = i; j <= s.length(); j++)
+		{
+			auto subStr = s.substr(i, j - i);
+			if (j == s.length() || subStr.find(s[j]) != string::npos)
+			{
+				minSize = subStr.length() > minSize ? subStr.length() : minSize;
+				break;
+			}
+		}
+	}
+
+	return minSize;
+}
+
 int main()
 {
 	cout << lengthOfLongestSubstring("abcabcbb") << endl;
